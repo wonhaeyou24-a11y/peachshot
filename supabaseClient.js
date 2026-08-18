e3730d9fbb26495a199032fd769181b20cdf6f84
wonhaeyou24 <wonhaeyou24@gmail.com>
2026-08-18 20:19:39 +0900

    Supabase 연동

---
+++ supabaseClient.js
@@ -0,0 +1,5 @@
+// Supaba// Supabase 라이브러리 초기화
+const SUPABASE_URL = 'https://sibblzsehmkchxyrprqt.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_3F3thDPZ2cz8b7Jd-PLL4A__WcY5J8h';
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
